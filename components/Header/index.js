7cf8acbf29530bb165638df7475eb0a3ae5bd8d5
Luce Carter <luce@lucyscarter.co.uk>
2021-04-18 20:37:37 +0100

    Moved to next Image and cleaned up header

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,61 +4,64 @@ import SocialLinks from "@components/SocialLinks";
 import DadJoke from "@components/DadJoke";
 import { useRouter } from "next/router";
 import { Config } from "@utils/Config";
+import Image from "next/image";
 
+const LOGO_URL = "https://images.ctfassets.net/f37i8279gxv6/1HJrK7EkbnZbp6IU4r0pTV/66f3b6a303e1793735850ed5dbcf8eb5/footer_logo.png";
 
 export default function Header() {
   const router = useRouter();
 
   return (
-    <header className={HeaderStyles.header}>
-      <div className={HeaderStyles.header__logoContainer}>
-        <Link href="/">
-          <a
-            className={HeaderStyles.header__logoContainerLink}
-            aria-label="Navigate to home page"
-          >
-            <img src="/logo.png" alt="Luce Carter Logo" className={HeaderStyles.header__logoBanner} />
-            {/* <Logo /> */}
-          </a>
-        </Link>
-      </div>
-      <div className={HeaderStyles.header__dadJokeContainer}>
-        <DadJoke />
-      </div>
-      
-      <nav className={HeaderStyles.header__nav} role="navigation">
-        <ul className={HeaderStyles.header__navList}>
-          {Config.menuLinks.map((link) => {
-            const onBlogPost =
-              router.pathname === Config.pageMeta.post.slug &&
-              link.path === Config.pageMeta.blogIndex.slug;
+    <>
+      <header className={HeaderStyles.header}>
+          <Link href="/">
+            <a
+              className={HeaderStyles.header__logoContainerLink}
+              aria-label="Navigate to home page"
+            >
 
-            const onBlogIndexPage =
-              router.pathname === Config.pageMeta.blogIndexPage.slug &&
-              link.path === Config.pageMeta.blogIndex.slug;
+              <Image src={LOGO_URL} alt="Luce Carter Logo" height="1080" width="1920" layout="responsive" />
+            </a>
+          </Link>
 
-            const isActive =
-              onBlogPost || onBlogIndexPage || router.pathname === link.path;
-            const isActiveClass = isActive
-              ? ` ${HeaderStyles.header__navListItem__active}`
-              : "";
+        <div className={HeaderStyles.header__dadJokeContainer}>
+          <DadJoke />
+        </div>
+        
+        <nav className={HeaderStyles.header__nav} role="navigation">
+          <ul className={HeaderStyles.header__navList}>
+            {Config.menuLinks.map((link) => {
+              const onBlogPost =
+                router.pathname === Config.pageMeta.post.slug &&
+                link.path === Config.pageMeta.blogIndex.slug;
 
-            return (
-              <li
-                key={link.displayName}
-                className={HeaderStyles.header__navListItem + isActiveClass}
-              >
-                <Link href={link.path}>
-                  <a className={HeaderStyles.header__navListItemLink}>
-                    {link.displayName}
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+              const onBlogIndexPage =
+                router.pathname === Config.pageMeta.blogIndexPage.slug &&
+                link.path === Config.pageMeta.blogIndex.slug;
+
+              const isActive =
+                onBlogPost || onBlogIndexPage || router.pathname === link.path;
+              const isActiveClass = isActive
+                ? ` ${HeaderStyles.header__navListItem__active}`
+                : "";
+
+              return (
+                <li
+                  key={link.displayName}
+                  className={HeaderStyles.header__navListItem + isActiveClass}
+                >
+                  <Link href={link.path}>
+                    <a className={HeaderStyles.header__navListItemLink}>
+                      {link.displayName}
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
       <SocialLinks />     
-    </header>
+  </>
   );
 }
